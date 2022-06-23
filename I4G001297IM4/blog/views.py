ed778233d6d2a6105a5b4c51a732d29c4cbe9d38
zeelyha <ngozi.priscillia29@gmail.com>
2022-06-24 00:23:29 +0800

    Created Views:
    Created List, Create, Detail, Update and Delete views

--- I4G001297IM4/blog/views.py
+++ I4G001297IM4/blog/views.py
@@ -1,3 +1,32 @@
-from django.shortcuts import render
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
+from django.urls import reverse_lazy
+
+from I4G001297IM4.blog.models import Post
 
 # Create your views here.
+class PostListView(ListView):
+    model = Post
+
+
+class PostCreateView(CreateView):
+    model = Post
+    fields = '__all__'
+    success_url = reverse_lazy("blog:all")
+
+
+class PostDetailView(DetailView):
+    model = Post
+
+
+class PostUpdateView(UpdateView):
+    model = Post
+    fields = "__all__"
+    success_url = reverse_lazy("blog:all")
+
+
+class PostDeleteView(DeleteView):
+    model = Post
+    fields = "__all__"
+    success_url = reverse_lazy("blog:all")
